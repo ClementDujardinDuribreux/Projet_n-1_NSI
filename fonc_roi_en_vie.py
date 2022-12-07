6f07e31dd6be9db450283851979e3a93577c2344
ClementDujardinDuribreux <dujardin-duribreux.clement@lpogustaveeiffel59.net>
2022-12-08 00:02:47 +0100

    Début du projet

---
+++ fonc_roi_en_vie.py
@@ -0,0 +1,5 @@
+def roi_en_vie_J1():
+    pass
+
+def roi_en_vie_J2():
+    pass
---
+++ fonction_lancement.py
@@ -0,0 +1,7 @@
+from fonc_roi_en_vie import roi_en_vie_J1, roi_en_vie_J2
+from fonc_pion_en_nombre import pions_en_nombre
+
+
+def lancer(J1: str, J2: str) -> None:
+    while roi_en_vie_J1() == True and roi_en_vie_J2 == True:
+        pass
