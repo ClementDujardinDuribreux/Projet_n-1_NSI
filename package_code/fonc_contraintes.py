148f2f98aefef292784cd9f9cc92ee69cc7844e3
Clément Dujardin-Duribreux <120065855+ClementDujardinDuribreux@users.noreply.github.com>
2022-12-14 13:55:36 +0100

    amelioration fonc_contraintes_pions

--- package_code/fonc_contraintes.py
+++ package_code/fonc_contraintes.py
@@ -83,6 +83,9 @@ def contraintes_pions(tour:int, dico_plateau:dict, pos_ini:tuple, pos_final:tupl
             elif pos_final not in (pos_ini[0] + 1, pos_ini[1]):
                 raison = 'Tu ne peux pas bouger ton pion ici !'
                 return (False, raison)
+            elif pos_final in (pos_ini[0] + 1, pos_ini[1]) and verifier_case(dico_plateau, pos_final) in joueur_2.values():
+                raison = 'Tu ne peux pas bouger ton pion ici car un autre pion te bloque !'
+                return (False, raison)
         elif joueur == 2:
             if pos_final in (pos_ini[0] - 1, pos_ini[1] + 1) and verifier_case(dico_plateau, (pos_ini[0] - 1, pos_ini[1] + 1)) not in joueur_1.values():
                 if verifier_case(dico_plateau, (pos_ini[0] - 1, pos_ini[1] + 1)) == '':
@@ -99,8 +102,10 @@ def contraintes_pions(tour:int, dico_plateau:dict, pos_ini:tuple, pos_final:tupl
             elif pos_final not in (pos_ini[0] - 1, pos_ini[1]):
                 raison = 'Tu ne peux pas bouger ton pion ici !'
                 return (False, raison)
+            elif pos_final in (pos_ini[0] - 1, pos_ini[1]) and verifier_case(dico_plateau, pos_final) in joueur_1.values():
+                raison = 'Tu ne peux pas bouger ton pion ici car un autre pion te bloque !'
+                return (False, raison)
     return (True, '')
-        
 
 ##  ------------------------------------------------------------  ##
 
